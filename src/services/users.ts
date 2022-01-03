9280ada1b32b3114ca1d60e4f7d59b1825759638
Oli Radlett <o.radlett@gmail.com>
2022-01-03 15:53:05 +0000

    Add initial basic get user by id route

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,24 +1,32 @@
 import { User } from "@models/user";
+import UserModel  from "../schemas/users";
 
 // A post request should not contain an id.
 export type UserCreationParams = Pick<User, "email" | "name" | "phoneNumbers">;
 
 export class UsersService {
-  public get(id: number, name?: string): User {
-    return {
-      id,
-      email: "jane@doe.com",
-      name: name ?? "Jane Doe",
-      status: "Happy",
-      phoneNumbers: [],
-    };
+  public async get(id: number, name?: string): Promise<User> {
+    
+    // Need to validate the user given id to prevent spooky stuff
+
+    // Need to do some TypeScript stuff to make this proper
+    return await UserModel.findOne({"id": id}).then((data) => {
+        
+      // Need to do some validation first
+
+      return data as User;
+
+    })
+    .catch((error: Error) => {
+      throw error;
+    });
+
   }
 
   public create(userCreationParams: UserCreationParams): User {
     return {
       id: Math.floor(Math.random() * 10000), // Random
-      status: "Happy",
-      ...userCreationParams,
+      ...userCreationParams
     };
   }
 }
