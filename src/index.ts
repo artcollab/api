7b832e39db8fc9f46a7a6fd33d698b6b52b74cd7
Oli Radlett <o.radlett@gmail.com>
2022-01-04 21:47:47 +0000

    Add error handling to api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import express, { Application } from "express";
+/* eslint-disable no-console */
+import express, { Application, Response as ExResponse, Request as ExRequest, NextFunction, } from "express";
+
 import morgan from "morgan";
 import nocache from "nocache";
 
 import { RegisterRoutes } from "@tsoa/routes";
+import { ValidateError } from "tsoa";
 
 import swaggerUi from "swagger-ui-express";
 import swaggerDoc from "@tsoa/swagger.json";
@@ -21,11 +24,9 @@ const uri = `mongodb://${dbUsername}:${dbPassword}@${dbURI}:${dbPort}/drawdojo?a
 
 mongoose.connect(uri);
 
-// eslint-disable-next-line no-console
 mongoose.connection.on('open', () => {console.log('Connected to mongo server.');});
 
 // TODO - Swap console.error for some proper logging
-// eslint-disable-next-line no-console
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.set("etag", false);
@@ -36,10 +37,38 @@ app.use(express.urlencoded());
 
 RegisterRoutes(app);
 
+app.use(function errorHandler(
+  err: unknown,
+  req: ExRequest,
+  res: ExResponse,
+  next: NextFunction
+): ExResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+
+  next();
+});
+
+app.use(function notFoundHandler(_req, res: ExResponse) {
+  res.status(404).send({
+    message: "Not Found",
+  });
+});
+
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`listening on port ${port}`);
 });
 
